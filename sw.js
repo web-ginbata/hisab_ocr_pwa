ad8a2bad8b1508ad54ae3a6992d2911636b0087f
web-ginbata <189704641+web-ginbata@users.noreply.github.com>
2025-06-23 21:42:02 +0300

    updt

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("ocr-cache-v1").then(cache =>
+    caches.open("ocr-cache-v0").then(cache =>
       cache.addAll([
         "./",
         "./index.html",
         "./manifest.json",
         "./style.css",
-        "./script.js",
         "./tesseract/tesseract.min.js",
-        "./tesseract/eng.traineddata.gz",
+        "./tesseract/eng.traineddata",  // ✅ updated
         "./assets/logo.png"
       ])
     )
